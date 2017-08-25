27c86eb69aece711196c80bfe66dad1c0419c12e
Robert Shaw <r.shaw1@yahoo.com>
2017-08-24 21:11:40 -0500

    cleaned up Nav component

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,35 +1,28 @@
-import React, { Component } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const views = ['Home', 'Campuses', 'Students']
 
-export default class Nav extends Component {
-  render() {
-    return (
-      // <div>
-      <nav className="navbar navbar-default">
-        <NavLink to="/">
-          <span className="navbar-brand" >Margaret Hamilton Academy</span>
-        </NavLink>
-        <div>
-          <ul className="nav nav-pills">
-            {views.map(view => {
-              let path;
-              if (view === 'Home') {
-                path = '/'
-              } else {
-                path = `/${view}`
-              }
-              return (
-                <NavLink className="nav nav-pills" to={`${path}`}>
-                  <li key={`${view}`} className="navBarList" ><span>{view}</span></li>
-                </NavLink>
-              )
-            })}
-          </ul>
-          {/*<h4 className="home-title">Margaret Hamilton Interplanetary Academy of JavaScript</h4>*/}
-        </div>
-      </nav>
-    )
-  }
+export default function Nav() {
+  return (
+    <nav className="navbar navbar-default">
+      <NavLink to="/">
+        <span className="navbar-brand" >
+          Margaret Hamilton Academy
+        </span>
+      </NavLink>
+      <div>
+        <ul className="nav nav-pills">
+          {views.map(view => {
+            let path = view === 'Home' ? '/' : `/${view}`;
+            return (
+              <NavLink className="nav nav-pills" to={path}>
+                <li key={view}>{view}</li>
+              </NavLink>
+            )
+          })}
+        </ul>
+      </div>
+    </nav>
+  )
 }
