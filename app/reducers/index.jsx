c66bf8c7a64f132ec90048fa9cbd06e302a53c82
Robert Shaw <r.shaw1@yahoo.com>
2017-08-27 23:09:52 -0500

    added Thunks and handle change actions

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,18 +1,43 @@
-import { combineReducers } from 'redux'
+// import { combineReducers } from 'redux'
+import axios from 'axios';
+
 
 //ACTION TYPES
 const GET_ALL_STUDENTS = 'GET_ALL_STUDENTS';
 const ADD_STUDENT = 'ADD_STUDENT';
 const DELETE_STUDENT = 'DELETE_STUDENT';
+const UPDATE_STUDENT = 'UPDATE_STUDENT';
 
 const GET_ALL_CAMPUSES = 'GET_ALL_CAMPUSES';
 const ADD_CAMPUS = 'ADD_CAMPUS';
+const DELETE_CAMPUS = 'DELETE_CAMPUS';
+const UPDATE_CAMPUS = 'UPDATE_CAMPUS';
+
+const EDIT_STUDENT_NAME = 'EDIT_STUDENT_NAME';
+const EDIT_STUDENT_EMAIL = 'EDIT_STUDENT_EMAIL';
+
+const EDIT_CAMPUS_NAME = 'EDIT_CAMPUS_NAME';
+
 
 //ACTION CREATORS
 export const getStudents = function (students) {
 	return {
 	type: GET_ALL_STUDENTS,
 	students: students
+	};
+};
+
+export const editStudentName = function (studentName) {
+	return {
+	type: EDIT_STUDENT_NAME,
+	studentName
+	};
+};
+
+export const editStudentEmail = function (studentEmail) {
+	return {
+	type: EDIT_STUDENT_EMAIL,
+	studentEmail
 	};
 };
 
@@ -23,10 +48,10 @@ export const addStudent = function (student) {
 	};
 };
 
-export const deleteStudent = function (student) {
+export const deleteStudent = function (deletedStudent) {
 	return {
 	type: DELETE_STUDENT,
-	student: student
+	deletedStudent: deletedStudent
 	};
 };
 
@@ -44,19 +69,113 @@ export const addCampus = function (campus) {
 	};
 };
 
+export const editCampusName = function (campusName) {
+	return {
+	type: EDIT_CAMPUS_NAME,
+	campusName
+	};
+};
+
+//THUNK CREATORS
+export const fetchStudents = function() {
+
+  //THUNK
+  return function thunk(dispatch, getState) {
+    axios.get('/api/students')
+    .then(res => res.data)
+    .then(students => {
+      const getStudentsAction = getStudents(students)
+      dispatch(getStudentsAction);
+    })
+  }
+}
+
+export const fetchCampuses = function() {
+
+  //THUNK
+  return function thunk(dispatch, getState) {
+    axios.get('/api/campuses')
+    .then(res => res.data)
+    .then(campuses => {
+      const getCampusesAction = getCampuses(campuses)
+      dispatch(getCampusesAction);
+    })
+  }
+}
+
+export const createCampus = function(name, image) {
+
+  //THUNK
+  return function thunk(dispatch, getState) {
+    axios.post('/api/campuses/addcampus', {name, image})
+    .then(res => res.data)
+    .then(campus => {
+      const addCampusAction = addCampus(campus)
+      dispatch(addCampusAction);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+}
+
+export const createStudent = function(name, email, assignedCampus) {
+
+  //THUNK
+  return function thunk(dispatch, getState) {
+    axios.post('/api/students/addstudent', {name, email, assignedCampus})
+    .then(res => res.data)
+    .then(student => {
+      const addStudentAction = addStudent(student)
+      dispatch(addStudentAction);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+}
+
+export const destroyStudent = function(studentName) {
+  
+  //THUNK
+  return function thunk(dispatch, getState) {
+    axios.delete(`/api/students/${studentName}`)
+    .then(res => res.data)
+    .then(deletedStudent => {
+      const deleteStudentAction = deleteStudent(deletedStudent)
+      dispatch(deleteStudentAction);
+    })
+    .then(() => {
+      let students = getState().students;
+      const getStudentsAction = getStudents(students)
+      dispatch(getStudentsAction);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+}
+
 
 //INITIAL STATE
 const initialState = {
   students: [],
   campuses: [],
   campus: {},
+  campusName: '',
   student: {},
+  deletedStudent: {},
+  studentName: '',
+  studentEmail: '',
 };
 
 
 //REDUCER
 const rootReducer = function(prevState = initialState, action) {
   const nextState = Object.assign({}, prevState);
+
+  let studentArr;
+  let index;
 
   switch(action.type) {
     case GET_ALL_STUDENTS:
@@ -66,15 +185,26 @@ const rootReducer = function(prevState = initialState, action) {
       nextState.campuses = action.campuses
       break
     case ADD_CAMPUS:
-      nextState.campus = action.campus
-      nextState.campuses.push(action.campus)
+      nextState.campuses = nextState.campuses.concat(action.campus)
+      break
+    case EDIT_CAMPUS_NAME:
+      nextState.campusName = action.campusName
+      break
+    case EDIT_STUDENT_NAME:
+      nextState.studentName = action.studentName
+      break
+    case EDIT_STUDENT_EMAIL:
+      nextState.studentEmail = action.studentEmail
       break
     case ADD_STUDENT:
-      nextState.student = action.student
-      nextState.students.push(action.student)
+      nextState.students = nextState.students.concat(action.student)
       break
     case DELETE_STUDENT:
-      nextState.student = action.student
+      studentArr = nextState.students.slice();
+      index = studentArr.findIndex(student => student.id === action.deletedStudent.id);
+      console.log('index = ', index)
+      studentArr.splice(index, 1);
+      nextState.students = studentArr;
       break
     default: return prevState
   }
