a78b086220c16a5254a41ba88c5417be90150700
Robert Shaw <r.shaw1@yahoo.com>
2017-08-28 23:08:38 -0500

    refactored campuses component to be a functional component using react-redux

--- app/components/Campus/Campuses.jsx
+++ app/components/Campus/Campuses.jsx
@@ -1,82 +1,66 @@
 //Node Modules
-import React, { Component } from 'react';
+import React from 'react';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import { Button, Card, Icon, Image } from 'semantic-ui-react';
 import faker from 'faker';
 
-//Local Modules
-import store from '../../store';
-import {fetchCampuses} from '../../reducers'
 
+function Campuses(props) {
+  const {campuses} = props;
 
-export default class Campuses extends Component {
-  constructor(props) {
-    super(props);
-    this.state = store.getState();
-  }
+  return (
+    <div>
+      <Link to="/Campuses/AddCampus">
+        <button className="addCampus">ADD A CAMPUS</button>
+      </Link>
 
-  componentDidMount() {
-    this.unsusbscribe = store.subscribe(() => {
-      this.setState(store.getState())
-    })
-    
-    const fetchCampusesThunk = fetchCampuses();
-    store.dispatch(fetchCampusesThunk);
-  }
+      <Card.Group >
+        {campuses.map(campus => {
+          let path = `/Campuses/${campus.name}`
+          let campusImg = campus.image ? campus.image : faker.image.city();
+          return (
+            <Card key={campus.id}>
+              <Link to={`${path}`}>
+                <Image src={campusImg} />
+              </Link>
+                <Card.Content>
+                  <Card.Header>
+                    <Link to={`${path}`}>
+                      {campus.name}
+                    </Link>
+                  </Card.Header>
+                <Card.Meta>
+                  <span className='date'>
+                    Established 2017
+                  </span>
+                </Card.Meta>
+                <Card.Description>
+                  Specialty is CSS
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a className='ui floated-right'>
+                  <Icon name='user' />
+                  22 Students
+                </a>
+                <Button size='mini' floated='right' color='blue'>
+                  Update Campus Info
+                </Button>
+              </Card.Content>
+            </Card>
+          )
+        })}
+      </Card.Group>
+    </div>
+  )
+}
 
-  componentWillUnmount() {
-    this.unsusbscribe();
-  }
-
-  render() {
-    const campuses = this.state.campuses;
-    // ---> can also be expressed as const {students} = this.state
-
-    return (
-      <div>
-        <Link to="/Campuses/AddCampus">
-          <button className="addCampus">ADD A CAMPUS</button>
-        </Link>
-
-        <Card.Group >
-          {campuses.map(campus => {
-            let path = `/Campuses/${campus.name}`
-            let campusImg = campus.image ? campus.image : faker.image.city();
-            return (
-              <Card key={campus.id}>
-                <Link to={`${path}`}>
-                  <Image src={campusImg} />
-                </Link>
-                  <Card.Content>
-                    <Card.Header>
-                      <Link to={`${path}`}>
-                        {campus.name}
-                      </Link>
-                    </Card.Header>
-                  <Card.Meta>
-                    <span className='date'>
-                      Established 2017
-                    </span>
-                  </Card.Meta>
-                  <Card.Description>
-                    Specialty is CSS
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <a className='ui floated-right'>
-                    <Icon name='user' />
-                    22 Students
-                  </a>
-                  <Button size='mini' floated='right' color='blue'>
-                    Update Campus Info
-                  </Button>
-                </Card.Content>
-              </Card>
-            )
-          })}
-        </Card.Group>
-      </div>
-    )
+const mapStateToProps = function(state) {
+  return {
+    campuses: state.campuses,
   }
 }
 
+const CampusesContainer = connect(mapStateToProps)(Campuses);
+export default CampusesContainer;
